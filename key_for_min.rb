6b7ef9e543dcb1a59b59f5401114c0aebcebff64
Cara Morelli <caramorelli7@gmail.com>
2018-09-25 02:04:53 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,8 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, val|
     if val < min 
       min = val
-      key_min = key
+      key_min = key 
+    end
   end 
-
+  return key_min
 end
