2d85319f6257440e5644fa128a1b381aa60c7a38
Cara Morelli <caramorelli7@gmail.com>
2018-09-25 02:09:33 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  print name_hash.first[0].class
+  print name_hash.first[0].to_s
   puts ''
   # min = name_hash[] 
   # key_min = nil
